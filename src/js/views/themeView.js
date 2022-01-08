cf571bd98fe9824d8f229cbecd69a5b2403adb71
aly2705 <alexandraaly.simion@gmail.com>
2022-01-09 00:19:51 +0200

    fixed issue

--- src/js/views/themeView.js
+++ src/js/views/themeView.js
@@ -1,14 +1,14 @@
 class ThemeView {
   _parentElement = document.documentElement;
-  _sliderBtn = document.querySelector('.slider');
+  _sliderBtn = document.querySelector('.slider__circle');
 
   setTheme(theme) {
-    this._parentElement.dataset.theme = theme;
-    this._sliderBtn.querySelector('.slider__circle').style.left =
+    document.documentElement.dataset.theme = theme;
+    document.querySelector('.slider__circle').style.left =
       theme === 'dark' ? '4.5rem' : '1rem';
   }
   addHandlerChangeTheme(handler) {
-    this._sliderBtn.addEventListener('click', function () {
+    document.querySelector('.slider').addEventListener('click', function () {
       const actualTheme = document.documentElement.dataset.theme;
       console.log(actualTheme);
       handler(actualTheme);
