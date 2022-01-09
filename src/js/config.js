d9eea41e9797fd9106ab3c6557ce2737c6777555
aly2705 <alexandraaly.simion@gmail.com>
2022-01-09 16:22:22 +0200

    changed api key

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 // dev key: a83a04be-2610-4bb1-864a-e9c13b429357
-export const KEY = '82d38576-8ae5-4aa5-9844-a391376cccfe';
+export const KEY = 'bd1cf07a-a0cb-44d7-8d16-0a85b599868a';
 export const MODAL_CLOSE_SEC = 2.5;
 export const SERVINGS_TO_UPLOAD = 4;
 export const SPOONACULAR_ENDPOINT = `https://api.spoonacular.com/recipes/parseIngredients`;
